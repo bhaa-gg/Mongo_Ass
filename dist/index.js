16c3f0bb61ed06fabb657d45607c4e88b1f6d48b
unknown <bhaatiti281@gmail.com>
2025-04-22 02:04:36 +0200

    final Deploy2

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_init_1 = require("./app-init");
 const dbConnection_1 = require("./Db/dbConnection");
-(0, app_init_1.main)();
 (0, dbConnection_1.dbConnection)();
+(0, app_init_1.main)();
--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import { main } from "./app-init";
 import { dbConnection } from "./Db/dbConnection";
 
 
-main()
 dbConnection()
+main()
